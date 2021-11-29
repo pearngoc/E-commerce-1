dd5432065cb9de6558f2ab24cc397b29cfee85e6
pearngoc <lethanhngoc123a@gmail.com>
2021-11-29 15:06:40 +0700

    show list product and filter  product base on category

--- component/products/productController.js
+++ component/products/productController.js
@@ -1,9 +1,11 @@
 const productService = require('./productService')
-
+const { mutipleMongooseToObject } = require('../../util/mongoose')
 
 class CourseController{
-    show(req, res, next){
-        const products = productService.show();
-        res.render('product/products', { products });
+    async show(req, res, next){
+        const products = await productService.show();
+        res.render('product/products', { products: mutipleMongooseToObject(products) });
     }
 }
+
+module.exports = new CourseController();
