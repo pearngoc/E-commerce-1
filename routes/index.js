8ee7209193fdd29cb41320eba2534ffaf6d62a12
Nguyen Tai <62838260+ntantai21810@users.noreply.github.com>
2022-01-06 22:09:20 +0700

    fix ui in index page

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,31 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const cartModel = require('../component/cart/cartModel')
+const cartModel = require("../component/cart/cartModel");
+const productService = require("../component/products/productService");
 /* GET home page. */
-router.get('/', async function(req, res, next) {
-  if(req.user){
-    const carts = await cartModel.findOne({customerID: req.user.id})
+router.get("/", async function (req, res, next) {
+  let top10Products = await productService.getTop10ProductsBySold();
+  if (req.user) {
+    const carts = await cartModel.findOne({ customerID: req.user.id });
+
     let totalItem = 0;
-    if(carts){
-      for(let item of carts.cart){
-        totalItem += item.qty
+
+    if (carts) {
+      for (let item of carts.cart) {
+        totalItem += item.qty;
       }
     }
-    if(req.session.cart){
-      for(let otherI in req.session.cart.items){
+    if (req.session.cart) {
+      for (let otherI in req.session.cart.items) {
         totalItem += req.session.cart.items[otherI].qty;
       }
     }
     req.user.totalItem = totalItem;
   }
-  res.render('index');
-});
 
+  res.render("index", {
+    top10Products,
+  });
+});
 
 module.exports = router;
